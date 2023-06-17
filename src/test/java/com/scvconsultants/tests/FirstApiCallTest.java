3fbeb90930bc4da1ed48a22ae26ecc9e6ed71e7b
Michal Dobrzycki <dobrzycki@gmail.com>
2023-06-18 00:09:00 +0200

    Jackson-databind i pierwszy test

--- src/test/java/com/scvconsultants/tests/FirstApiCallTest.java
+++ src/test/java/com/scvconsultants/tests/FirstApiCallTest.java
@@ -1,2 +1,13 @@
-package com.scvconsultants.tests;public class FirstApiCallTest {
+package com.scvconsultants.tests;
+
+import io.restassured.RestAssured;
+import org.testng.annotations.Test;
+
+public class FirstApiCallTest {
+    @Test
+    public void getFirstApiCall() {
+        RestAssured
+                .get("https://api.github.com/")
+                .prettyPeek();
+    }
 }
